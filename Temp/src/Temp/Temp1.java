cf8f8845e2d61189f3f1e1bb273e04814e667fd0
30401 <30401@DESKTOP-9MC4BVB>
2020-04-16 15:47:53 +0900

    dd

---
+++ Temp/src/Temp/Temp1.java
@@ -0,0 +1,15 @@
+package Temp;
+
+public class Temp1 {
+
+	public static void main(String[] args) {
+		System.out.println("안녕하세요");
+		System.out.println("황병헌입니다.");
+		System.out.println("오늘은" + 1 + "일차" + "수업입니다");
+		System.out.println("3 + 4 = " + 3 + 4);
+		System.out.println("3 + 4 = " + (3 + 4));
+		System.out.println("3 * 4 = " + 3 * 4);
+
+	}
+
+}
---
+++ Temp/src/Temp/Third.java
@@ -0,0 +1,12 @@
+package Temp;
+
+public class Third {
+
+	public static void main(String args[]) {
+		System.out.println(args[0]);
+		System.out.println(args[1]);
+		System.out.println(args[2]);
+
+	}
+
+}
